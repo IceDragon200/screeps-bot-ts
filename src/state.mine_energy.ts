288fb497867a0661f352407e453fc3850aaa878d
Corey Powell <mistdragon100@gmail.com>
2016-12-27 11:35:22 -0500

    More handlers for mine energy

--- src/state.mine_energy.ts
+++ src/state.mine_energy.ts
@@ -1,11 +1,21 @@
+import Hive from "./hive";
+
 namespace MineEnergyStep {
 	export function run(creep: Creep, currentState) {
-		const source = <Source>creep.pos.findClosestByPath(FIND_SOURCES, {
+		const source = <Source>Hive.findBy(creep.pos, FIND_SOURCES, {
 			ignoreCreeps: false
 		});
 		if (source) {
-			if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-				creep.moveTo(source);
+			switch (creep.harvest(source)) {
+				case ERR_NOT_IN_RANGE:
+					creep.moveTo(source);
+					break;
+				case ERR_NO_BODYPART:
+					creep.memory.role = 'idler';
+					break;
+				case ERR_NOT_ENOUGH_RESOURCES:
+					creep.memory.sleeping = 5;
+					break;
 			}
 			creep.memory.idle = 0;
 		} else {
